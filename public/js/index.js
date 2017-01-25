1936d8b64b89e40c4b6ae783ce3f1f3707f9682c
AravindVasudev <aravindv96@yahoo.com>
2017-01-25 08:49:49 +0530

    Background Restored

--- public/js/index.js
+++ public/js/index.js
@@ -3,27 +3,28 @@ $(function() {
   var url = 'http://unsplash.com/rss';
 
   $.ajax({
-  url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent(url),
-  async: false,
-  jsonpCallback: 'jsonCallback',
-  contentType: "application/json",
-  dataType: 'jsonp',
-  success: function(json) {
+    url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent(url),
+    async: false,
+    jsonpCallback: 'jsonCallback',
+    contentType: "application/json",
+    dataType: 'jsonp',
+    success: function(json) {
 
-    var min = 0;
-    var max = json.responseData.feed.entries.length;
-    // and the formula is:
-    var random = Math.floor(Math.random() * (max - min + 1)) + min;
+      var min = 0;
+      var max = json.responseData.feed.entries.length;
+      // and the formula is:
+      var random = Math.floor(Math.random() * (max - min + 1)) + min;
 
-    var str = json.responseData.feed.entries[random].content;
+      var str = json.responseData.feed.entries[random].content;
 
-    var regex = /<img.*?src="(.*?)"/;
-    var src = regex.exec(str)[1];
+      var regex = /<img.*?src="(.*?)"/;
+      var src = regex.exec(str)[1];
 
-    $('body').css('background-image','url('+src+')');
+      $('body').css('background-image','url('+src+')');
 
-  }
-  });
+    }
+  })
+
   $(".enter-trig").focus();
 
   $(".advanced").one("click", function() {
