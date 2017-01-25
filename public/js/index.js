263e7ec948a1cd002ca4fb88fc35133eb287a73e
AravindVasudev <aravindv96@yahoo.com>
2017-01-25 08:44:58 +0530

    Restored Background

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,39 @@
 $(function() {
+
+  var url = 'http://unsplash.com/rss';
+
+  $.ajax({
+  url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent(url),
+  async: false,
+  jsonpCallback: 'jsonCallback',
+  contentType: "application/json",
+  dataType: 'jsonp',
+  success: function(json) {
+
+    var min = 0;
+    var max = json.responseData.feed.entries.length;
+    // and the formula is:
+    var random = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    var str = json.responseData.feed.entries[random].content;
+
+    var regex = /<img.*?src="(.*?)"/;
+    var src = regex.exec(str)[1];
+
+    $('body').css('background-image','url('+src+')');
+
+  }
+  });
   $(".enter-trig").focus();
 
   $(".advanced").one("click", function() {
     $(this).empty();
     $("#advanced").fadeIn();
   });
-  
+
   //ajaxURL.js
   new Clipboard('.copy');
-  
+
   $("#submit-code").one("click", function() {
     $("#submit-code").css('backgroundColor', '#E0E0E0');
     $.ajax({
@@ -38,12 +63,12 @@ $(function() {
       }
     });
   });
-  
+
   //submitWithEnter.js
   $(".enter-trig").keypress(function(event) {
-    
+
     if(event.which == 13)
       $(".trigg").trigger("click");
   });
-   
+
 });
